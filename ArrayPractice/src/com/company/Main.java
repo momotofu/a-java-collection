b95be108f73920fff4adeb5483edc8e73df5155e
Christopher Reece <mrkita.reece@gmail.com>
2017-10-05 11:48:47 +0900

    added custom print array method

--- ArrayPractice/src/com/company/Main.java
+++ ArrayPractice/src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
+
+    public static void printArray(int[] array) {
+        String outPut = "[";
+
+        for (int i = 0; i < array.length; i++) {
+            outPut += array[i];
+
+            if (i == array.length - 1) {
+                outPut += "]";
+            } else {
+                outPut += ", ";
+            }
+        }
+        System.out.println(outPut);
+    }
 
     public static void main(String[] args) {
 	// write your code here
+
+        int[] array1;
+        int[] array2 = new int[4];
+        int[] array3 = {5, 2, 3, 9, 1};
+
+        String[] stringArray1 = {"bananas", "apples", "oranges", "mangos", "kiwis"};
+        printArray(array3);
     }
 }
