b1dd8b031982724127a657c8bb77481fe9c13ed3
Christopher Reece <mrkita.reece@gmail.com>
2017-10-07 17:18:17 +0900

    added summation recursive function

--- RecursionPractice/src/com/company/Main.java
+++ RecursionPractice/src/com/company/Main.java
@@ -2,6 +2,15 @@ package com.company;
 
 public class Main {
 
+    public static int summation(int n) {
+        // base case
+        if (n <= 0) {
+            return 0;
+        } else {
+            return n + summation(n - 1);
+        }
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
