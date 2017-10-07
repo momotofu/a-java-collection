9de0c908a1fdf312799c12f59f821674e8d0e656
Christopher Reece <mrkita.reece@gmail.com>
2017-10-07 17:44:08 +0900

    added power function

--- RecursionPractice/src/com/company/Main.java
+++ RecursionPractice/src/com/company/Main.java
@@ -7,11 +7,33 @@ public class Main {
         if (n <= 0) {
             return 0;
         } else {
+            // recursive case
             return n + summation(n - 1);
+        }
+    }
+
+    public static int factorial(int n) {
+        // base case
+        if (n <= 1) {
+            return 1;
+        } else {
+            // recursive case
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static int power(int n, int p) {
+        // base case
+        if (p <= 0) {
+            return 1;
+        } else {
+            // recursive case
+            return n * power(n, p - 1);
         }
     }
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(power(5,5));
     }
 }
