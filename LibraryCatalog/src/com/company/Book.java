cc833d84f8cb7045ad03414fb5edbab0d32e5896
Christopher Reece <mrkita.reece@gmail.com>
2017-10-10 10:47:29 +0900

    created a constructor for the book class

--- LibraryCatalog/src/com/company/Book.java
+++ LibraryCatalog/src/com/company/Book.java
@@ -1,4 +1,18 @@
 package com.company;
 
 public class Book {
+
+    String title;
+    int pageCount;
+    int ISBN;
+    boolean isCheckedOut;
+    int dayCheckedOut = -1;
+
+    // Constructor
+    public Book(String title, int pageCount, int ISBN) {
+        this.title = title;
+        this.pageCount = pageCount;
+        this.ISBN = ISBN;
+        isCheckedOut = false;
+    }
 }
