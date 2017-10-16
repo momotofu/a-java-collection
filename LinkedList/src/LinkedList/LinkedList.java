229d842b6e6bf909d547d1b4e365b936585d84c3
Christopher Reece <mrkita.reece@gmail.com>
2017-10-16 18:34:24 +0900

    implemented LinkedList class constructors

--- LinkedList/src/LinkedList/LinkedList.java
+++ LinkedList/src/LinkedList/LinkedList.java
@@ -2,8 +2,19 @@ package LinkedList;
 
 public class LinkedList {
     // Properties
+    Node head;
+    int count;
 
     // Constructors
+    public LinkedList() {
+        head = null;
+        count = 0;
+    }
+
+    public LinkedList(Node head) {
+        this.head = head;
+        count = 1;
+    }
 
     // Methods
 }
