2a8d0330d0395a2359bcb5cf3f663083661a736f
Christopher Reece <mrkita.reece@gmail.com>
2017-10-16 18:25:49 +0900

    wrote psuedo code and getters and setters

--- LinkedList/src/LinkedList/Node.java
+++ LinkedList/src/LinkedList/Node.java
@@ -4,6 +4,9 @@ public class Node {
     Node next;
     int data;
 
+    // Methods
+
+    // Constructors
     public Node(int data) {
         this.data = data;
         next = null;
@@ -12,5 +15,23 @@ public class Node {
     public Node(int data, Node next) {
         this.data = data;
         this.next = next;
+    }
+
+    // Getters
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public void setData(int data) {
+        this.data = data
+    }
+
+    // Setters
+    public Node getNext() {
+        return this.next;
+    }
+
+    public int getInt() {
+        return this.data;
     }
 }
