7d61f59e6d78db1ab939c1b9a0aa4e97b842831e
Christopher Reece <mrkita.reece@gmail.com>
2017-10-16 18:18:45 +0900

    created constructors for node class

--- LinkedList/src/LinkedList/Node.java
+++ LinkedList/src/LinkedList/Node.java
@@ -1,4 +1,16 @@
 package LinkedList;
 
 public class Node {
+    Node next;
+    int data;
+
+    public Node(int data) {
+        this.data = data;
+        next = null;
+    }
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
 }
