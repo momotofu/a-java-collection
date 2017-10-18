ddec4dc9699b18bd7532bbaa3933cdee103896de
Christopher Reece <mrkita.reece@gmail.com>
2017-10-18 12:33:58 +0900

    testing try catch statement

--- ExceptionsPractice/src/com/company/Main.java
+++ ExceptionsPractice/src/com/company/Main.java
@@ -6,5 +6,14 @@ public class Main {
 	// write your code here
         int[] array = new int[10];
         System.out.println(array);
+
+        try {
+            int[] c = new int[5];
+            System.out.println("Element 6 at index 5 is: " + c[5]);
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception thrown: " + e);
+        }
+
+        System.out.println("Finally finished try catch");
     }
 }
