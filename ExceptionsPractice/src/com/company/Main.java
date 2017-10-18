e2be3675d373f03bfac25f37cfacf44a0dc1714b
Christopher Reece <mrkita.reece@gmail.com>
2017-10-18 12:42:49 +0900

    reformated block and added final block

--- ExceptionsPractice/src/com/company/Main.java
+++ ExceptionsPractice/src/com/company/Main.java
@@ -7,13 +7,14 @@ public class Main {
         int[] array = new int[10];
         System.out.println(array);
 
+        // Exceptions safe guard your code against bad input
         try {
-            int[] c = new int[5];
+            int[] c = new int[6];
             System.out.println("Element 6 at index 5 is: " + c[5]);
         } catch(ArrayIndexOutOfBoundsException e) {
             System.out.println("Exception thrown: " + e);
+        } finally {
+            System.out.println("Finally finished try catch");
         }
-
-        System.out.println("Finally finished try catch");
     }
 }
