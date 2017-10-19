b8434a3b5be973e7307f840e44b9211110992bcd
Christopher Reece <mrkita.reece@gmail.com>
2017-10-19 17:02:36 +0900

    added stack trace to exception control flow

--- HotChocolate/src/com/company/Main.java
+++ HotChocolate/src/com/company/Main.java
@@ -4,14 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        double currentLiquidTemp = 170;
+        double currentLiquidTemp = 160;
 
         try {
-            
+            HotChocolate.drink(currentLiquidTemp);
         } catch(TooHotException error) {
-
+            System.out.println("Ouwwee! It's too hot.");
+            error.printStackTrace();
         } catch(TooColdException error) {
-
+            System.out.println("Spleh! It's too cold.");
+            error.printStackTrace();
+        } catch (TemperatureException e) {
+            e.printStackTrace();
         }
     }
 }
