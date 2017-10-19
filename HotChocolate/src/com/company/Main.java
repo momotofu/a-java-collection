c5bcb511fea8a01e17857d8666c24b7ae64d1bc5
Christopher Reece <mrkita.reece@gmail.com>
2017-10-19 17:23:31 +0900

    implented while loop

--- HotChocolate/src/com/company/Main.java
+++ HotChocolate/src/com/company/Main.java
@@ -4,19 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        double currentLiquidTemp = 200;
-        boolean wrongTemp = true;
+        double currentLiquidTemp = 130;
+        boolean wrongTemp = !(currentLiquidTemp >= 180 && currentLiquidTemp <= 160);
 
-        try {
-            HotChocolate.drink(currentLiquidTemp);
-        } catch(TooHotException error) {
-            System.out.println("Ouwwee! It's too hot.");
-            error.printStackTrace();
-        } catch(TooColdException error) {
-            System.out.println("Spleh! It's too cold.");
-            error.printStackTrace();
-        } catch (TemperatureException e) {
-            e.printStackTrace();
+        while(wrongTemp) {
+            try {
+                HotChocolate.drink(currentLiquidTemp);
+                wrongTemp = false;
+            } catch(TooHotException error) {
+                System.out.println("Ouwwee! It's too hot.");
+                currentLiquidTemp -= 5;
+            } catch(TooColdException error) {
+                System.out.println("Spleh! It's too cold.");
+                currentLiquidTemp += 5;
+            } catch (TemperatureException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
