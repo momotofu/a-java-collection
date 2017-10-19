b99e3377914af34a28bb516aeae514201a4d6ea2
Christopher Reece <mrkita.reece@gmail.com>
2017-10-19 16:56:26 +0900

    filling out stub code

--- HotChocolate/src/com/company/Main.java
+++ HotChocolate/src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        double currentLiquidTemp = 170;
+
+        try {
+            
+        } catch(TooHotException error) {
+
+        } catch(TooColdException error) {
+
+        }
     }
 }
