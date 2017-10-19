71de66118a1998c327cb9b8ee63ba876e88c96b2
Christopher Reece <mrkita.reece@gmail.com>
2017-10-19 16:51:19 +0900

    added control flow to drink method

--- HotChocolate/src/com/company/HotChocolate.java
+++ HotChocolate/src/com/company/HotChocolate.java
@@ -4,4 +4,12 @@ public class HotChocolate {
 
     public static final double tooHot = 185;
     public static final double tooCold = 160;
+
+    public static void drink(double temp) throws TemperatureException {
+        if (temp >= tooHot) {
+            throw new TooHotException();
+        } else if (temp <= tooCold) {
+            throw new TooColdException();
+        }
+    }
 }
