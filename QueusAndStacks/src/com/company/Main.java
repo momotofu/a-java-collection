66ea671646f5498a383ac29a07aedf3bbf402240
Christopher Reece <mrkita.reece@gmail.com>
2017-10-21 15:25:55 +0900

    added puts method to main

--- QueusAndStacks/src/com/company/Main.java
+++ QueusAndStacks/src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
 public class Main {
-
+    public static void puts(Object obj) {
+        System.out.println(obj);
+    }
+    
     public static void main(String[] args) {
 	// write your code here
+        Queue Q = new Queue();
+
+        Q.isEmpty()
     }
 }
