c12a20c41ac6a55dd661a9d4e45dcce32970fefe
Christopher Reece <mrkita.reece@gmail.com>
2017-10-21 15:28:29 +0900

    tested out queue data structure

--- QueusAndStacks/src/com/company/Main.java
+++ QueusAndStacks/src/com/company/Main.java
@@ -4,11 +4,18 @@ public class Main {
     public static void puts(Object obj) {
         System.out.println(obj);
     }
-    
+
     public static void main(String[] args) {
 	// write your code here
         Queue Q = new Queue();
 
-        Q.isEmpty()
+        puts(Q.isEmpty());
+        Q.enqueue(5);
+        Q.enqueue(9);
+        Q.enqueue(6);
+        Q.enqueue(1);
+        puts(Q.peek());
+        puts(Q.dequeue());
+        puts(Q.size());
     }
 }
