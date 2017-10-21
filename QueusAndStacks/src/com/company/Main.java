53f41682225c393a3797f4f7f7a06322484c6de3
Christopher Reece <mrkita.reece@gmail.com>
2017-10-21 15:55:36 +0900

    messed around with stacks LIFO"

--- QueusAndStacks/src/com/company/Main.java
+++ QueusAndStacks/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Stack;
+
 public class Main {
     public static void puts(Object obj) {
         System.out.println(obj);
@@ -7,15 +9,31 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Queue Q = new Queue();
+        // FIFO & LIFO
+//        Queue Q = new Queue();
+//
+//        puts(Q.isEmpty());
+//        Q.enqueue(5);
+//        Q.enqueue(9);
+//        Q.enqueue(6);
+//        Q.enqueue(1);
+//        puts(Q.peek());
+//        puts(Q.dequeue());
+//        puts(Q.size());
 
-        puts(Q.isEmpty());
-        Q.enqueue(5);
-        Q.enqueue(9);
-        Q.enqueue(6);
-        Q.enqueue(1);
-        puts(Q.peek());
-        puts(Q.dequeue());
-        puts(Q.size());
+        // LIFO
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        puts("size of stack: " + stack.size());
+        stack.push(4);
+        stack.push(5);
+        puts("peek: " + stack.peek());
+        puts("pop: " + stack.pop());
+        puts("pop: " + stack.pop());
+        puts("pop: " + stack.pop());
+        puts("pop: " + stack.pop());
+        puts("pop: " + stack.pop());
     }
 }
