08c8fa9e0ea4e4456c6e9e37b77cc8986d505da4
Christopher Reece <mrkita.reece@gmail.com>
2017-10-24 17:22:17 +0900

    created person in main

--- Person/src/com/company/Main.java
+++ Person/src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Person PeterParker = new Person();
+        Person SpiderMan = PeterParker;
     }
 }
