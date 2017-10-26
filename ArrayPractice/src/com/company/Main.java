b8a9e075e3db773054c91e19adc1af452f426a33
Christopher Reece <mrkita.reece@gmail.com>
2017-10-26 19:57:46 +0900

    changed Int to integer

--- ArrayPractice/src/com/company/Main.java
+++ ArrayPractice/src/com/company/Main.java
@@ -6,16 +6,16 @@ public class Main {
 
     public static <E> void printArray(E[] array) {
         for (E element : array) {
-            System.out.println(element);
+            System.out.println(element + " ");
         }
     }
 
     public static void main(String[] args) {
 	// write your code here
 
-        int[] array1;
-        int[] array2 = new int[4];
-        int[] array3 = {5, 2, 3, 9, 1};
+        Integer[] array1;
+        Integer[] array2 = new Integer[4];
+        Integer[] array3 = {5, 2, 3, 9, 1};
 
         String[] stringArray1 = {"bananas", "apples", "oranges", "mangos", "kiwis"};
         printArray(array3);
