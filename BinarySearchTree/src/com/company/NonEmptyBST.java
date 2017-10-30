ff7f601f603fb47ccd01ca966b6700cb23466ff3
Christopher Reece <mrkita.reece@gmail.com>
2017-10-30 19:31:30 +0900

    implemented NonEmptyBST data structure

--- BinarySearchTree/src/com/company/NonEmptyBST.java
+++ BinarySearchTree/src/com/company/NonEmptyBST.java
@@ -1,7 +1,15 @@
 package com.company;
 
 public class NonEmptyBST<D extends Comparable> implements Tree<D> {
+    D data;
+    Tree<D> left;
+    Tree<D> right;
 
-    
+    // Constructor
+    public NonEmptyBST(D el) {
+        data = el;
+        left = new EmptyBST();
+        right = new EmptyBST();
+    }
 
 }
