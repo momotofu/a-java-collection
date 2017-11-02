9de72822796feeb23e1efe2a44bcf04594177d90
Christopher Reece <mrkita.reece@gmail.com>
2017-11-03 00:32:16 +0900

    implemented setBoard func

--- TicTacToeApplication/src/com/company/TicTacToe.java
+++ TicTacToeApplication/src/com/company/TicTacToe.java
@@ -5,7 +5,25 @@ public class TicTacToe {
     // Properties
     protected char[] board;
     protected char userMarker;
-    protected char AIMarker;
+    protected char AiMarker;
     protected char winner;
     protected char currentMarker;
+
+    // Constructor
+    public TicTacToe(char userMarker, char AiMarker) {
+        self.userMarker = userMarker;
+        self.AiMarker = AiMarker;
+        self.winner = '-';
+        self.setBoard();
+    }
+
+    public char[] setBoard() {
+        char[] board = new char[9];
+
+        for (int i = 0; i < board.length; i++) {
+            board[i] = '-';
+        }
+
+        return board;
+    }
 }
