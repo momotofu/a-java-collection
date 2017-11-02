f4997c08809020147d570db42e6dbccce45c1592
Christopher Reece <mrkita.reece@gmail.com>
2017-11-03 00:24:42 +0900

    added properties

---
+++ TicTacToeApplication/src/com/company/TicTacToe.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class TicTacToe {
+
+    // Properties
+    protected char[] board;
+    protected char userMarker;
+    protected char AIMarker;
+    protected char winner;
+    protected char currentMarker;
+}
