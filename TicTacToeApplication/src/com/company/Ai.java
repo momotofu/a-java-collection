006442f515a7d8e8d5edce9d02d000001b3b7757
Christopher Reece <mrkita.reece@gmail.com>
2017-11-03 13:08:58 +0900

    finished implementing ai

--- TicTacToeApplication/src/com/company/Ai.java
+++ TicTacToeApplication/src/com/company/Ai.java
@@ -1,18 +1,20 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Ai {
 
-    public void pickSpot(char[] board) {
-          ArrayList<Integer> arrayList = new ArrayList();
+    public int pickSpot(char[] board) {
+          ArrayList<Integer> choices = new ArrayList();
 
           for (int i = 0; i < board.length; i++) {
               if (board[i] == '-') {
-                  arrayList.add(i);
+                  choices.add(i + 1);
               }
           }
 
-
+          Random rand = new Random();
+          return choices.get(Math.abs(rand.nextInt() % choices.size()));
     }
 }
