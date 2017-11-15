7ab0409ee4c498176baabf59f19e57895854eaab
Christopher Reece <mrkita.reece@gmail.com>
2017-11-15 11:35:40 +0900

    fleshed out initialize streams

--- Hangman/src/com/company/HangmanGame.java
+++ Hangman/src/com/company/HangmanGame.java
@@ -1,9 +1,12 @@
 package com.company;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import static com.company.Main.puts;
 
 public class HangmanGame {
 
@@ -15,7 +18,7 @@ public class HangmanGame {
     int maxTries = 6;
     int currentTry = 0;
 
-    ArrayList<String> dictionary = new ArrayList<>();
+    ArrayList<String> wordList = new ArrayList<>();
     private static FileReader fileReader;
     private static BufferedReader bufferedReader;
 
@@ -27,7 +30,23 @@ public class HangmanGame {
 
     // Methods
     public void initializeStreams() throws IOException {
-        
+        try {
+            File inFile = new File("dictionary.txt");
+            fileReader = new FileReader(inFile);
+            bufferedReader = new BufferedReader(fileReader);
+
+            // populate dictionary with words
+            String currentLine = bufferedReader.readLine();
+            while (currentLine != null) {
+                wordList.add(currentLine);
+            }
+
+            // close file readers
+            fileReader.close();
+            bufferedReader.close();
+        } catch (IOException error) {
+            puts("Could not init streams with error: " + error.toString());
+        }
     }
 
     public String pickWord() {
