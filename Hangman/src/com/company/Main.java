d2b5b7411cf5c1376c4e72c7f1906adc443c96f0
Christopher Reece <mrkita.reece@gmail.com>
2017-11-15 10:45:56 +0900

    overloaded print helper

--- Hangman/src/com/company/Main.java
+++ Hangman/src/com/company/Main.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void puts(Object input) {
-        if (input == null) { input = "";}
         System.out.println(input);
+    }
+
+    public static void puts() {
+        System.out.println();
     }
 
     public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
 	    puts("Welcome to Hangman!");
+	    puts("I will pick a word. If you can guess that word in six your less tries you win.");
+	    puts("Are you ready?");
+	    puts();
 
     }
 }
