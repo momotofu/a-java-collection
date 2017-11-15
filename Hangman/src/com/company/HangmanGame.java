87546244c99cfa3d60f056d705d442a6f3a85f6f
Christopher Reece <mrkita.reece@gmail.com>
2017-11-15 11:12:46 +0900

    fleshed out hangmanGame constructor

--- Hangman/src/com/company/HangmanGame.java
+++ Hangman/src/com/company/HangmanGame.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class HangmanGame {
 
     // Properties
     String mysteryWord;
     StringBuilder currentGuess;
+    ArrayList<Character> previousGuess = new ArrayList<>();
+
+    int maxTries = 6;
+    int currentTry = 0;
+
+    ArrayList<String> dictionary = new ArrayList<>();
+    private static FileReader fileReader;
+    private static BufferedReader bufferedReader;
+
+    public HangmanGame() throws IOException {
+        initializeStreams();
+        mysteryWord = pickWord();
+        currentGuess = initializeCurrentGuess();
+    }
+
+    
 }
