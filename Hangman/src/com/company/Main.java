613e93e2af0ab081b3be1e73661c170f3e720dcd
Christopher Reece <mrkita.reece@gmail.com>
2017-11-15 11:00:01 +0900

    fleshing out game loop logic

--- Hangman/src/com/company/Main.java
+++ Hangman/src/com/company/Main.java
@@ -13,11 +13,31 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        // Properties
+        boolean isPlaying = true;
+
+        // Game instructions
 	    Scanner scanner = new Scanner(System.in);
 	    puts("Welcome to Hangman!");
 	    puts("I will pick a word. If you can guess that word in six your less tries you win.");
 	    puts("Are you ready?");
 	    puts();
 
+        // Enter game loop
+        while (isPlaying) {
+            puts();
+
+            while (!game.gameOver()) {
+
+            }
+
+            // prompt the user for another game
+            puts("Do you want to play another game?"
+            + " Enter Y for yes or N for No.");
+
+            // Exit or continue game loop
+            Character answer = scanner.next().toUpperCase().charAt(0);
+            isPlaying = (answer == 'Y');
+        }
     }
 }
