d1200962e82f13710846df36d7a1094179351266
Christopher Reece <mrkita.reece@gmail.com>
2017-11-17 15:22:38 +0900

    started find number of repititions function

--- RuntimePractice/src/com/company/Main.java
+++ RuntimePractice/src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 public class Main {
 
+    public static int findNumberOfRepititions() {
+
+    }
+    
     public static void main(String[] args) {
 	// write your code here
         long startTime = System.currentTimeMillis();
