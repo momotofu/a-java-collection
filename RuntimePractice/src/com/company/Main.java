6c0a907e326e93cf70f5f84e7e6a7be2e68dd5b8
Christopher Reece <mrkita.reece@gmail.com>
2017-11-17 17:13:58 +0900

    started next repitition function

--- RuntimePractice/src/com/company/Main.java
+++ RuntimePractice/src/com/company/Main.java
@@ -13,11 +13,15 @@ public class Main {
         return sum;
     }
 
+    public static int findNumberOfRepititionsV1(String s, char c) {
+
+    }
+
     public static void main(String[] args) {
 	// write your code here
         long startTime = System.currentTimeMillis();
         long endTime = System.currentTimeMillis();
         long duration = endTime - startTime;
-        System.out.println("Duration: " + duration);
+        System.out.println("Duration: " + duration + "ms");
     }
 }
