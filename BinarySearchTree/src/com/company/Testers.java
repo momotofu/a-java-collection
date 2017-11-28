e476185118678e109c29d326c5c774a61224d98b
Christopher Reece <mrkita.reece@gmail.com>
2017-11-28 19:03:50 +0900

    starting checkIsEmpty unit test

--- BinarySearchTree/src/com/company/Testers.java
+++ BinarySearchTree/src/com/company/Testers.java
@@ -1,5 +1,10 @@
 package com.company;
 
 public class Testers {
-    
+
+    public static void checkIsEmpty(Tree t) {
+        // if instance of t is an EmptyBST then it should return true
+
+        // otherwise it should return false
+    }
 }
