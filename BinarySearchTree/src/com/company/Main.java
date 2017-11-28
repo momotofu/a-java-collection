ac94a24646832ba814249890b55282dce27933e9
Christopher Reece <mrkita.reece@gmail.com>
2017-11-28 19:11:53 +0900

    tested unit test in main

--- BinarySearchTree/src/com/company/Main.java
+++ BinarySearchTree/src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        EmptyBST e = new EmptyBST();
+        NonEmptyBST f = new NonEmptyBST(10);
+
+        System.out.println(Testers.checkIsEmpty(e));
+        System.out.println(Testers.checkIsEmpty(f));
     }
 }
