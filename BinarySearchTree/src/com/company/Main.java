47fc57589e5264a369991aafc6a53fb23e0cbdc7
Christopher Reece <mrkita.reece@gmail.com>
2017-11-28 19:16:58 +0900

    added an exception

--- BinarySearchTree/src/com/company/Main.java
+++ BinarySearchTree/src/com/company/Main.java
@@ -2,12 +2,12 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 	// write your code here
         EmptyBST e = new EmptyBST();
         NonEmptyBST f = new NonEmptyBST(10);
 
-        System.out.println(Testers.checkIsEmpty(e));
-        System.out.println(Testers.checkIsEmpty(f));
+        Testers.checkIsEmpty(e);
+        Testers.checkIsEmpty(f);
     }
 }
--- BinarySearchTree/src/com/company/Testers.java
+++ BinarySearchTree/src/com/company/Testers.java
@@ -2,13 +2,13 @@ package com.company;
 
 public class Testers {
 
-    public static boolean checkIsEmpty(Tree t) {
+    public static boolean checkIsEmpty(Tree t) throws Exception {
         // if instance of t is an EmptyBST then it should return true
         // otherwise it should return false
 
         if (t instanceof EmptyBST) {
-            if (t.isEmpty()) {
-                return true;
+            if (!t.isEmpty()) {
+                throw new Exception("EmptyBST should return true");
             } else {
                 return false;
             }
