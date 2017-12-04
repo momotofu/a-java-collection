ddcce9f6cb91038020680bd526bdac23ef773cf7
Christopher Reece <mrkita.reece@gmail.com>
2017-12-04 18:08:41 +0900

    changes

--- BinarySearchTree/src/com/company/Main.java
+++ BinarySearchTree/src/com/company/Main.java
@@ -5,20 +5,14 @@ public class Main {
     public static void main(String[] args) throws Exception {
 	// write your code here
         EmptyBST e = new EmptyBST();
-        NonEmptyBST f = new NonEmptyBST(5);
+        NonEmptyBST f = new NonEmptyBST(1);
 
         System.out.println(f.cardinality());
-        f.add(21);
-        f.add(1);
-        f.add(4);
-        f.add(6);
+        f.add(2);
+        f.add(3);
         System.out.println(f.cardinality());
         System.out.println(f.right);
         System.out.println(f.left);
-        System.out.println(f.member(21));
-        System.out.println(f.member(5));
-        Testers.checkIsEmpty(e);
-        Testers.checkIsEmpty(f);
 
 //        Testers.checkAddMemberCardinality(e, 5);
 //        Testers.checkAddMemberCardinality(f, 5);
