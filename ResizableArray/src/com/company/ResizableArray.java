74a1bce2619062174c4289144532ba520d4f45af
Christopher Reece <mrkita.reece@gmail.com>
2017-12-14 19:45:16 +0900

    implemented ResizableArray class

--- ResizableArray/src/com/company/ResizableArray.java
+++ ResizableArray/src/com/company/ResizableArray.java
@@ -1,6 +1,39 @@
 package com.company;
 
 public class ResizableArray {
-    private int[] items;
-    private int[] size;
+    private int[] items = new int[8];
+    private int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public void set(int index, int item) {
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+        items[index] = item;
+    }
+
+    public void append(int item) {
+        ensureItemCapacity();
+        items[size] = item;
+        size++;
+
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+        return items[index];
+    }
+
+    public void ensureItemCapacity() {
+        if (size == items.length) {
+            int[] copy = new int[size * 2];
+            System.arraycopy(items, 0, copy, 0, size);
+            items = copy;
+        }
+    }
 }
