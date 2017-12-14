0b3ee94ddaf5b33f846caf493c50f996b210fc6d
Christopher Reece <mrkita.reece@gmail.com>
2017-12-14 19:33:48 +0900

    created ResizableArray class

---
+++ ResizableArray/src/com/company/ResizableArray.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class ResizableArray {
+    private int[] items;
+    private int[] size;
+}
--- User/src/com/company/Main.java
+++ User/src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Set a = new HashSet();
+        a.add(1101);
+
+        User chris = new User("chris", "password", 28, a);
     }
 }
